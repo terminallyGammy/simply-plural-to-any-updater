5e3c4cb9dd01a235c3e3fb3115234084671d1556
GollyTicker <golly.ticker@gmail.com>
2025-06-03 18:50:22 +0200

    quality: prepare integration tests

--- test.sh
+++ test.sh
@@ -2,4 +2,22 @@
 
 set -euo pipefail
 
+# unit tests
 cargo test --bin sps_status
+
+
+# integration test
+# cargo build --bin sps_status --release
+
+# todo. add integration tests with new test system here.
+# 1. set system fronts
+# 2. start webserver
+# 3. check correct system fronts on website
+# 4. change fronts
+# 5. check new fronts on website
+
+# 1. set system fronts
+# 2. start VRC updater
+# 3. check vrc status correct after a few seconds of startup
+# 4. change fronts
+# 5. check vrc status correct after 60 seconds

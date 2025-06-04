d8795052579e7c2b12dc99b59bdda724b174b5ea
GollyTicker <golly.ticker@gmail.com>
2025-06-04 10:39:09 +0200

    test: VRC Updater integration test wip.

--- README.md
+++ README.md
@@ -48,6 +48,7 @@ All functionality is implemented using Rust and various libraries.
 For developers, one can use `dev.*.run.sh` for local quick running.
 
 And run the files in `test` for testing. For the integration tests,
-you'll need to export the `SPS_API_TOKEN` and `SPS_API_WRITE_TOKEN` of the plural system used for tests.
+you'll need to export the `SPS_API_TOKEN` and `SPS_API_WRITE_TOKEN` of the plural system used for tests - 
+as well as `VRCHAT_USERNAME`, `VRCHAT_PASSWORD` and `VRCHAT_COOKIE` of the VRC test user.
 
 To create a release, simply push a corresponding tag - e.g. `v1.2.3`.
--- test/vrchatupdater.integration-tests.sh
+++ test/vrchatupdater.integration-tests.sh
@@ -1,8 +1,84 @@
 # integration test
 # cargo build --bin sps_status --release
 
-# 1. set system fronts
-# 2. start VRC updater
-# 3. check vrc status correct after a few seconds of startup
-# 4. change fronts
-# 5. check vrc status correct after 60 seconds
+#!/bin/bash
+
+set -euo pipefail
+
+[[ "$SPS_API_TOKEN" != "" ]]
+
+[[ "$SPS_API_WRITE_TOKEN" != "" ]]
+
+source ./test/plural_system_to_test.sh
+
+main() {
+    stop_vrc_updater
+
+    set_system_fronts_set "A"
+    
+    start_vrc_updater
+
+    # check_system_fronts_set "A"
+    
+    # set_system_fronts_set "B"
+
+    # sleep 60s
+
+    # check_system_fronts_set "B"
+
+    stop_vrc_updater
+
+    clear_all_fronts
+
+    # echo "✅✅✅ VRC Updater Integration Test ✅✅✅"
+}
+
+check_system_fronts_set() {
+    SET="$1"
+
+    HTML="$(curl -s "$WEBSERVER_FRONTING_URL")"
+
+    if [[ "$SET" == "A" ]]; then
+        grep '<title>SP-Updater-Test - Fronting Status</title>' <<< "$HTML"
+        grep '<div><img src="https://example.com/a" /><p>Annalea 💖 A.</p></div>' <<< "$HTML"
+        grep '<div><img src="https://example.com/b" /><p>Borgnen 👍 B.</p></div>' <<< "$HTML"
+        grep '<div><img src="" /><p>Daenssa 📶 D.</p></div>' <<< "$HTML"
+        [[ "$( grep '<div>' <<< "$HTML" | wc -l )" == "3" ]]
+    elif [[ "$SET" == "B" ]]; then
+        grep '<title>SP-Updater-Test - Fronting Status</title>' <<< "$HTML"
+        grep '<div><img src="" /><p>tešt ▶️ t.</p></div>' <<< "$HTML"
+        [[ "$( grep '<div>' <<< "$HTML" | wc -l )" == "1" ]]
+    else
+        return 1
+    fi
+}
+
+
+start_vrc_updater() {
+    cargo build --bin sps_status --release
+
+    rm -rf vrcupdater.env || true
+    
+    export SPS_API_TOKEN
+    export VRCHAT_USERNAME
+    export VRCHAT_PASSWORD
+    export VRCHAT_COOKIE
+
+    echo "SPS_API_TOKEN=\"$SPS_API_TOKEN\"" >> vrcupdater.env
+    echo "VRCHAT_USERNAME=\"$VRCHAT_USERNAME\"" >> vrcupdater.env
+    echo "VRCHAT_PASSWORD=\"$VRCHAT_PASSWORD\"" >> vrcupdater.env
+    echo "VRCHAT_COOKIE=\"$VRCHAT_COOKIE\"" >> vrcupdater.env
+
+    (./target/release/sps_status 2>&1 | sed 's/^/VRC Updater | /g' ) & 
+
+    sleep 5s
+
+    echo "Started VRC Updater."
+}
+
+stop_vrc_updater() {
+    pkill -f sps_status || true
+    echo "Stopped VRC Updater."
+}
+
+main

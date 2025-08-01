bdf8fc21e221529ebd03126bba508e253d647651
GollyTicker <golly.ticker@gmail.com>
2025-08-01 15:46:49 +0200

    Fix tests

--- release/lint.sh
+++ release/lint.sh
@@ -4,10 +4,8 @@ set -euo pipefail
 
 rustfmt --edition 2021 src/**.rs
 
-# NOTE! Sync -W args with vscode settings when changd
 cargo clippy --allow-dirty --fix -- \
     -W clippy::pedantic \
     -W clippy::nursery \
     -W clippy::unwrap_used \
     -W clippy::expect_used \
-# NOTE! Sync -W args this with vscode settings when changd
--- test/source.sh
+++ test/source.sh
@@ -7,10 +7,10 @@ export SECONDS_BETWEEN_UPDATES=10
 
 write_env_vars_to_config_json() {
     echo "{
-        \"simply_plural_token\": \"$SPS_API_TOKEN\",
-        \"vrchat_username\": \"$VRCHAT_USERNAME\",
-        \"vrchat_password\": \"$VRCHAT_PASSWORD\",
-        \"vrchat_cookie\": \"$VRCHAT_COOKIE\",
+        \"simply_plural_token\": \"${SPS_API_TOKEN-null}\",
+        \"vrchat_username\": \"${VRCHAT_USERNAME-null}\",
+        \"vrchat_password\": \"${VRCHAT_PASSWORD-null}\",
+        \"vrchat_cookie\": \"${VRCHAT_COOKIE-null}\",
         \"system_name\": \"${SYSTEM_PUBLIC_NAME-null}\",
         \"wait_seconds\": { \"secs\": ${SECONDS_BETWEEN_UPDATES}, \"nanos\": 0 }
     }" > "$CONFIG_FILE"
